cf3c268e7c582f1f33aa621b829c64ded26c5740
Simon <asedem.games@gmail.com>
2022-01-16 16:49:50 +0100

    PX-02: Bug fixed, while enabling the Database Classes

--- src/main/java/de/sns/database/entity/Koerperbauanalyse.java
+++ src/main/java/de/sns/database/entity/Koerperbauanalyse.java
@@ -1,11 +1,15 @@
 package de.sns.database.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table (name = "`Koerperbauanalyse`")
 public class Koerperbauanalyse {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     // TODO: Fragen und bearbeiten
     // TODO: Umbenennen
