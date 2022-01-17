1766d971c8984e801d139fe6b610faea14885b1d
Sam <sam.kolter@gmx.de>
2022-01-17 16:43:39 +0100

    Main: Created Columns in Classes Beschwerden and Ziele

--- src/main/java/de/sns/database/entity/Beschwerden.java
+++ src/main/java/de/sns/database/entity/Beschwerden.java
@@ -14,10 +14,95 @@ public class Beschwerden {
     @Column (name = "akuteBeschwerden", nullable = false)
     private String akuteBeschwerden;
 
-    // TODO: Alle booleans hinzufügen, die für beschwerden stehen
-
     @Column (name = "hauptBeschwerden", nullable = false)
     private String hauptBeschwerden;
+
+    @Column (name = "kopfHals", nullable = false)
+    private boolean kopfHals;
+
+    @Column (name = "schulter", nullable = false)
+    private boolean schulter;
+
+    @Column (name = "handEllenbogenArm", nullable = false)
+    private boolean handEllenbogenArm;
+
+    @Column (name = "beckenHuefte", nullable = false)
+    private boolean beckenHuefte;
+
+    @Column (name = "oberschenkelKnie", nullable = false)
+    private boolean oberschenkelKnie;
+
+    @Column (name = "unterschenkelFuss", nullable = false)
+    private boolean unterschenkelFuss;
+
+    @Column (name = "halsWirbelSaeule", nullable = false)
+    private boolean halsWirbelSaeule;
+
+    @Column (name = "brustWirbelSaeule", nullable = false)
+    private boolean brustWirbelSaeule;
+
+    @Column (name = "lendenWirbelSaeule", nullable = false)
+    private boolean lendenWirbelSaeule;
+
+    @Column (name = "haltungsschwaeche", nullable = false)
+    private boolean haltungsschwaeche;
+
+    @Column (name = "wirbelverschleiss", nullable = false)
+    private boolean wirbelverschleiss;
+
+    @Column (name = "osteoporose", nullable = false)
+    private boolean osteoporose;
+
+    @Column (name = "wirbelverschiebung", nullable = false)
+    private boolean wirbelverschiebung;
+
+    @Column (name = "bandscheibenschaden", nullable = false)
+    private boolean bandscheibenschaden;
+
+    @Column (name = "skoliose", nullable = false)
+    private boolean skoliose;
+
+    @Column (name = "baenderrisse", nullable = false)
+    private boolean baenderrisse;
+
+    @Column (name = "knochenbrueche", nullable = false)
+    private boolean knochenbrueche;
+
+    @Column (name = "muskelMuskelfaserRisse", nullable = false)
+    private boolean muskelMuskelfaserRisse;
+
+    @Column (name = "kreislaufschwaeche", nullable = false)
+    private boolean kreislaufschaeche;
+
+    @Column (name = "durchblutungsstoerungen", nullable = false)
+    private boolean durchblutungsstoerungen;
+
+    @Column (name = "krampfadernVenenschwaeche", nullable = false)
+    private boolean krampfadernVenenschwaeche;
+
+    @Column (name = "herzerkrankungen", nullable = false)
+    private boolean herzerkrankungen;
+
+    @Column (name = "nicotinkonsum", nullable = false)
+    private boolean nicotinkonsum;
+
+    @Column (name = "diabetes", nullable = false)
+    private boolean diabetes;
+
+    @Column (name = "bluthochdruck", nullable = false)
+    private boolean bluthochdruck;
+
+    @Column (name = "cholesterin", nullable = false)
+    private boolean cholesterin;
+
+    @Column (name = "schilddrueseUeberUnterfunktion", nullable = false)
+    private boolean schilddrueseUeberUnterfunktion;
+
+    @Column (name = "allergien", nullable = false)
+    private boolean allergien;
+
+    @Column (name = "medikamente", nullable = false)
+    private boolean medikamente;
 
     public Integer getId() {
         return id;
@@ -41,5 +126,237 @@ public class Beschwerden {
 
     public void setHauptBeschwerden(String hauptBeschwerden) {
         this.hauptBeschwerden = hauptBeschwerden;
+    }
+
+    public boolean isKopfHals() {
+        return kopfHals;
+    }
+
+    public void setKopfHals(boolean kopfHals) {
+        this.kopfHals = kopfHals;
+    }
+
+    public boolean isSchulter() {
+        return schulter;
+    }
+
+    public void setSchulter(boolean schulter) {
+        this.schulter = schulter;
+    }
+
+    public boolean isHandEllenbogenArm() {
+        return handEllenbogenArm;
+    }
+
+    public void setHandEllenbogenArm(boolean handEllenbogenArm) {
+        this.handEllenbogenArm = handEllenbogenArm;
+    }
+
+    public boolean isBeckenHuefte() {
+        return beckenHuefte;
+    }
+
+    public void setBeckenHuefte(boolean beckenHuefte) {
+        this.beckenHuefte = beckenHuefte;
+    }
+
+    public boolean isOberschenkelKnie() {
+        return oberschenkelKnie;
+    }
+
+    public void setOberschenkelKnie(boolean oberschenkelKnie) {
+        this.oberschenkelKnie = oberschenkelKnie;
+    }
+
+    public boolean isUnterschenkelFuss() {
+        return unterschenkelFuss;
+    }
+
+    public void setUnterschenkelFuss(boolean unterschenkelFuss) {
+        this.unterschenkelFuss = unterschenkelFuss;
+    }
+
+    public boolean isHalsWirbelSaeule() {
+        return halsWirbelSaeule;
+    }
+
+    public void setHalsWirbelSaeule(boolean halsWirbelSaeule) {
+        this.halsWirbelSaeule = halsWirbelSaeule;
+    }
+
+    public boolean isBrustWirbelSaeule() {
+        return brustWirbelSaeule;
+    }
+
+    public void setBrustWirbelSaeule(boolean brustWirbelSaeule) {
+        this.brustWirbelSaeule = brustWirbelSaeule;
+    }
+
+    public boolean isLendenWirbelSaeule() {
+        return lendenWirbelSaeule;
+    }
+
+    public void setLendenWirbelSaeule(boolean lendenWirbelSaeule) {
+        this.lendenWirbelSaeule = lendenWirbelSaeule;
+    }
+
+    public boolean isHaltungsschwaeche() {
+        return haltungsschwaeche;
+    }
+
+    public void setHaltungsschwaeche(boolean haltungsschwaeche) {
+        this.haltungsschwaeche = haltungsschwaeche;
+    }
+
+    public boolean isWirbelverschleiss() {
+        return wirbelverschleiss;
+    }
+
+    public void setWirbelverschleiss(boolean wirbelverschleiss) {
+        this.wirbelverschleiss = wirbelverschleiss;
+    }
+
+    public boolean isOsteoporose() {
+        return osteoporose;
+    }
+
+    public void setOsteoporose(boolean osteoporose) {
+        this.osteoporose = osteoporose;
+    }
+
+    public boolean isWirbelverschiebung() {
+        return wirbelverschiebung;
+    }
+
+    public void setWirbelverschiebung(boolean wirbelverschiebung) {
+        this.wirbelverschiebung = wirbelverschiebung;
+    }
+
+    public boolean isBandscheibenschaden() {
+        return bandscheibenschaden;
+    }
+
+    public void setBandscheibenschaden(boolean bandscheibenschaden) {
+        this.bandscheibenschaden = bandscheibenschaden;
+    }
+
+    public boolean isSkoliose() {
+        return skoliose;
+    }
+
+    public void setSkoliose(boolean skoliose) {
+        this.skoliose = skoliose;
+    }
+
+    public boolean isBaenderrisse() {
+        return baenderrisse;
+    }
+
+    public void setBaenderrisse(boolean baenderrisse) {
+        this.baenderrisse = baenderrisse;
+    }
+
+    public boolean isKnochenbrueche() {
+        return knochenbrueche;
+    }
+
+    public void setKnochenbrueche(boolean knochenbrueche) {
+        this.knochenbrueche = knochenbrueche;
+    }
+
+    public boolean isMuskelMuskelfaserRisse() {
+        return muskelMuskelfaserRisse;
+    }
+
+    public void setMuskelMuskelfaserRisse(boolean muskelMuskelfaserRisse) {
+        this.muskelMuskelfaserRisse = muskelMuskelfaserRisse;
+    }
+
+    public boolean isKreislaufschaeche() {
+        return kreislaufschaeche;
+    }
+
+    public void setKreislaufschaeche(boolean kreislaufschaeche) {
+        this.kreislaufschaeche = kreislaufschaeche;
+    }
+
+    public boolean isDurchblutungsstoerungen() {
+        return durchblutungsstoerungen;
+    }
+
+    public void setDurchblutungsstoerungen(boolean durchblutungsstoerungen) {
+        this.durchblutungsstoerungen = durchblutungsstoerungen;
+    }
+
+    public boolean isKrampfadernVenenschwaeche() {
+        return krampfadernVenenschwaeche;
+    }
+
+    public void setKrampfadernVenenschwaeche(boolean krampfadernVenenschwaeche) {
+        this.krampfadernVenenschwaeche = krampfadernVenenschwaeche;
+    }
+
+    public boolean isHerzerkrankungen() {
+        return herzerkrankungen;
+    }
+
+    public void setHerzerkrankungen(boolean herzerkrankungen) {
+        this.herzerkrankungen = herzerkrankungen;
+    }
+
+    public boolean isNicotinkonsum() {
+        return nicotinkonsum;
+    }
+
+    public void setNicotinkonsum(boolean nicotinkonsum) {
+        this.nicotinkonsum = nicotinkonsum;
+    }
+
+    public boolean isDiabetes() {
+        return diabetes;
+    }
+
+    public void setDiabetes(boolean diabetes) {
+        this.diabetes = diabetes;
+    }
+
+    public boolean isBluthochdruck() {
+        return bluthochdruck;
+    }
+
+    public void setBluthochdruck(boolean bluthochdruck) {
+        this.bluthochdruck = bluthochdruck;
+    }
+
+    public boolean isCholesterin() {
+        return cholesterin;
+    }
+
+    public void setCholesterin(boolean cholesterin) {
+        this.cholesterin = cholesterin;
+    }
+
+    public boolean isSchilddrueseUeberUnterfunktion() {
+        return schilddrueseUeberUnterfunktion;
+    }
+
+    public void setSchilddrueseUeberUnterfunktion(boolean schilddrueseUeberUnterfunktion) {
+        this.schilddrueseUeberUnterfunktion = schilddrueseUeberUnterfunktion;
+    }
+
+    public boolean isAllergien() {
+        return allergien;
+    }
+
+    public void setAllergien(boolean allergien) {
+        this.allergien = allergien;
+    }
+
+    public boolean isMedikamente() {
+        return medikamente;
+    }
+
+    public void setMedikamente(boolean medikamente) {
+        this.medikamente = medikamente;
     }
 }
--- src/main/java/de/sns/database/entity/Ziele.java
+++ src/main/java/de/sns/database/entity/Ziele.java
@@ -11,5 +11,299 @@ public class Ziele {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    // TODO: String einfügen
+
+    @Column (name = "kraftK3", nullable = false)
+    private String kraftK3;
+
+    @Column (name = "kraftKa", nullable = false)
+    private String kraftKa;
+
+    @Column (name = "ausdauerK3", nullable = false)
+    private String ausdauerK3;
+
+    @Column (name = "ausdauerKa", nullable = false)
+    private String ausdauerKa;
+
+    @Column (name = "beweglichkeitK3", nullable = false)
+    private String beweglichkeitK3;
+
+    @Column (name = "beweglichkeitKa", nullable = false)
+    private String beweglichkeitKa;
+
+    @Column (name = "gewichtK3", nullable = false)
+    private String gewichtK3;
+
+    @Column (name = "gewichtKa", nullable = false)
+    private String gewichtKa;
+
+    @Column (name = "schmerzK3", nullable = false)
+    private String schmerzK3;
+
+    @Column (name = "schmerzKa", nullable = false)
+    private String schmerzKa;
+
+    @Column (name = "balanceK3", nullable = false)
+    private String balanceK3;
+
+    @Column (name = "balanceKa", nullable = false)
+    private String balanceKa;
+
+    @Column (name = "sonstigesK", nullable = false)
+    private String sontigesK;
+
+    @Column (name = "kraftT3", nullable = false)
+    private String kraftT3;
+
+    @Column (name = "kraftTa", nullable = false)
+    private String kraftTa;
+
+    @Column (name = "ausdauerT3", nullable = false)
+    private String ausdauerT3;
+
+    @Column (name = "ausdauerTa", nullable = false)
+    private String ausdauerTa;
+
+    @Column (name = "beweglichkeitT3", nullable = false)
+    private String beweglichkeitT3;
+
+    @Column (name = "beweglichkeitTa", nullable = false)
+    private String beweglichkeitTa;
+
+    @Column (name = "gewichtT3", nullable = false)
+    private String gewichtT3;
+
+    @Column (name = "gewichtTa", nullable = false)
+    private String gewichtTa;
+
+    @Column (name = "schmerzT3", nullable = false)
+    private String schmerzT3;
+
+    @Column (name = "schmerzTa", nullable = false)
+    private String schmerzTa;
+
+    @Column (name = "balanceT3", nullable = false)
+    private String balanceT3;
+
+    @Column (name = "balanceTa", nullable = false)
+    private String balanceTa;
+
+    @Column (name = "sonstigesT", nullable = false)
+    private String sontigesT;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getKraftK3() {
+        return kraftK3;
+    }
+
+    public void setKraftK3(String kraftK3) {
+        this.kraftK3 = kraftK3;
+    }
+
+    public String getKraftKa() {
+        return kraftKa;
+    }
+
+    public void setKraftKa(String kraftKa) {
+        this.kraftKa = kraftKa;
+    }
+
+    public String getAusdauerK3() {
+        return ausdauerK3;
+    }
+
+    public void setAusdauerK3(String ausdauerK3) {
+        this.ausdauerK3 = ausdauerK3;
+    }
+
+    public String getAusdauerKa() {
+        return ausdauerKa;
+    }
+
+    public void setAusdauerKa(String ausdauerKa) {
+        this.ausdauerKa = ausdauerKa;
+    }
+
+    public String getBeweglichkeitK3() {
+        return beweglichkeitK3;
+    }
+
+    public void setBeweglichkeitK3(String beweglichkeitK3) {
+        this.beweglichkeitK3 = beweglichkeitK3;
+    }
+
+    public String getBeweglichkeitKa() {
+        return beweglichkeitKa;
+    }
+
+    public void setBeweglichkeitKa(String beweglichkeitKa) {
+        this.beweglichkeitKa = beweglichkeitKa;
+    }
+
+    public String getGewichtK3() {
+        return gewichtK3;
+    }
+
+    public void setGewichtK3(String gewichtK3) {
+        this.gewichtK3 = gewichtK3;
+    }
+
+    public String getGewichtKa() {
+        return gewichtKa;
+    }
+
+    public void setGewichtKa(String gewichtKa) {
+        this.gewichtKa = gewichtKa;
+    }
+
+    public String getSchmerzK3() {
+        return schmerzK3;
+    }
+
+    public void setSchmerzK3(String schmerzK3) {
+        this.schmerzK3 = schmerzK3;
+    }
+
+    public String getSchmerzKa() {
+        return schmerzKa;
+    }
+
+    public void setSchmerzKa(String schmerzKa) {
+        this.schmerzKa = schmerzKa;
+    }
+
+    public String getBalanceK3() {
+        return balanceK3;
+    }
+
+    public void setBalanceK3(String balanceK3) {
+        this.balanceK3 = balanceK3;
+    }
+
+    public String getBalanceKa() {
+        return balanceKa;
+    }
+
+    public void setBalanceKa(String balanceKa) {
+        this.balanceKa = balanceKa;
+    }
+
+    public String getSontigesK() {
+        return sontigesK;
+    }
+
+    public void setSontigesK(String sontigesK) {
+        this.sontigesK = sontigesK;
+    }
+
+    public String getKraftT3() {
+        return kraftT3;
+    }
+
+    public void setKraftT3(String kraftT3) {
+        this.kraftT3 = kraftT3;
+    }
+
+    public String getKraftTa() {
+        return kraftTa;
+    }
+
+    public void setKraftTa(String kraftTa) {
+        this.kraftTa = kraftTa;
+    }
+
+    public String getAusdauerT3() {
+        return ausdauerT3;
+    }
+
+    public void setAusdauerT3(String ausdauerT3) {
+        this.ausdauerT3 = ausdauerT3;
+    }
+
+    public String getAusdauerTa() {
+        return ausdauerTa;
+    }
+
+    public void setAusdauerTa(String ausdauerTa) {
+        this.ausdauerTa = ausdauerTa;
+    }
+
+    public String getBeweglichkeitT3() {
+        return beweglichkeitT3;
+    }
+
+    public void setBeweglichkeitT3(String beweglichkeitT3) {
+        this.beweglichkeitT3 = beweglichkeitT3;
+    }
+
+    public String getBeweglichkeitTa() {
+        return beweglichkeitTa;
+    }
+
+    public void setBeweglichkeitTa(String beweglichkeitTa) {
+        this.beweglichkeitTa = beweglichkeitTa;
+    }
+
+    public String getGewichtT3() {
+        return gewichtT3;
+    }
+
+    public void setGewichtT3(String gewichtT3) {
+        this.gewichtT3 = gewichtT3;
+    }
+
+    public String getGewichtTa() {
+        return gewichtTa;
+    }
+
+    public void setGewichtTa(String gewichtTa) {
+        this.gewichtTa = gewichtTa;
+    }
+
+    public String getSchmerzT3() {
+        return schmerzT3;
+    }
+
+    public void setSchmerzT3(String schmerzT3) {
+        this.schmerzT3 = schmerzT3;
+    }
+
+    public String getSchmerzTa() {
+        return schmerzTa;
+    }
+
+    public void setSchmerzTa(String schmerzTa) {
+        this.schmerzTa = schmerzTa;
+    }
+
+    public String getBalanceT3() {
+        return balanceT3;
+    }
+
+    public void setBalanceT3(String balanceT3) {
+        this.balanceT3 = balanceT3;
+    }
+
+    public String getBalanceTa() {
+        return balanceTa;
+    }
+
+    public void setBalanceTa(String balanceTa) {
+        this.balanceTa = balanceTa;
+    }
+
+    public String getSontigesT() {
+        return sontigesT;
+    }
+
+    public void setSontigesT(String sontigesT) {
+        this.sontigesT = sontigesT;
+    }
 }
